798a5be6577337cdcdd76eb8462835f1ac3fa47c
nincogamin <vlonpande@gmail.com>
2021-04-19 18:28:39 +0530

    First Commit

---
+++ src/app.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8000
+const path = require('path')
+const hbs = require('hbs')
+
+const publicPath = path.join(__dirname, "../public")
+const templatePath = path.join(__dirname, "../template/views")
+const partialsPath = path.join(__dirname, "../template/partials")
+
+app.use(express.static(publicPath))
+app.set('view engine', 'hbs')
+app.set('views', templatePath)
+hbs.registerPartials(partialsPath)
+
+// Routing
+app.get("/", (req, res) => {
+    res.render('index.hbs')
+})
+
+app.get("/about", (req, res) => {
+    res.render('about.hbs')
+})
+
+app.get("/games", (req, res) => {
+    res.render("games.hbs")
+})
+
+app.get("*", (req, res) => {
+    res.render('404Error.hbs')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on Port: ${port}`)
+})
